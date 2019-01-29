c92bb2bf50415bc775e99daafda6d283c7928023
Alex Peterson <apeterson4451@gmail.com>
2019-01-28 21:41:02 -0500

    Variable mutation and type coercion

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,4 +1,8 @@
-var firstName = 'John';
+/******************************
+* Variables and data types
+*/
+
+/*var firstName = 'John';
 console.log(firstName);
 
 var lastName = 'Smith';
@@ -14,5 +18,29 @@ console.log(job);
 
 job = 'Teacher';
 console.log(job);
-
+// Variable naming rules
 var _3years = 3;
+
+/***************************************
+ * Variable mutation and type coercion
+ */
+
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
